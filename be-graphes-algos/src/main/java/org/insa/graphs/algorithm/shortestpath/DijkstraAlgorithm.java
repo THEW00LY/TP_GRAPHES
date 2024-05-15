59d65aca875bd32c468f12ca98c30d96989527ca
Amine <amine.hajji1357@gmail.com>
2024-05-15 16:42:20 +0200

    ajout de la classe label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Node;
@@ -12,60 +13,110 @@ import org.insa.graphs.model.Path;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
-    public DijkstraAlgorithm(ShortestPathData data) {
+    public DijkstraAlgorithm(ShortestPathData data) { 
         super(data);
+    }
+
+
+    public boolean isMarque() {
+        return this.marque;
+    }
+
+    public void setMarque(boolean marque) {
+        this.marque = marque;
     }
 
     @Override
     protected ShortestPathSolution doRun() {
-        // Retrieve the graph.
-        ShortestPathData data = getInputData();
-        Graph graph = data.getGraph();
+        final ShortestPathData data = getInputData();
+        ShortestPathSolution solution = null;
+        ArrayList<Node> tas = new ArrayList<Node>();
 
-        final int nbNodes = graph.size();
+        Graph graph = data.getGraph();
+        Node origin = data.getOrigin();
 
         // Initialize array of distances.
-        double[] distances = new double[nbNodes]; 
+        double[] distances = new double[graph.size()];
         Arrays.fill(distances, Double.POSITIVE_INFINITY);
-        distances[data.getOrigin().getId()] = 0;
-
-        // Notify observers about the first event (origin processed).
-        notifyOriginProcessed(data.getOrigin());
+        distances[origin.getId()] = 0;
 
         // Initialize array of predecessors.
-        Arc[] predecessorArcs = new Arc[nbNodes]; 
+        Arc[] predecessorArcs = new Arc[graph.size()];
+        Arrays.fill(predecessorArcs, null);
 
-        Node Firstnode = data.getOrigin();
-        ArrayList<Node> Noeuds = new ArrayList<Node>();
-        Noeuds.add(Firstnode);
-        while(!Noeuds.isEmpty()) {
-        	Node x = Noeuds.get(0);
-        	Noeuds.remove(0);
-        	for(Arc arc : x.getSuccessors()) {
-        		Node y = arc.getDestination();
-        		if(!data.isAllowed(arc)) {
-        			continue;
-        		}
-        		double oldDistance = distances[y.getId()];
-        		double newDistance = distances[x.getId()] + data.getCost(arc);
-        		if(Double.isInfinite(oldDistance) && Double.isFinite(newDistance)) {
-        			notifyNodeReached(y);
-        		}
-        		if(newDistance < oldDistance) {
-        			distances[y.getId()] = newDistance;
-        			predecessorArcs[y.getId()] = arc;
-        			if(Noeuds.contains(y)) {
-        				Noeuds.remove(y);
-        			}
-        			Noeuds.add(y);
-        		}
-        	}
+        // Initialize the binary heap
+        BinaryHeap<Node> heap = new BinaryHeap<Node>();
+        tas.add(origin);
+        heap.insert(origin);
+        origin.setMarque(true);
+
+        notifyOriginProcessed(origin);
+
+
+        while (!heap.isEmpty()) {
+            Node node = heap.deleteMin();
+            node.setMarque(false);
+            notifyNodeMarked(node);
+
+            for (Arc arc : node.getSuccessors()) {
+                Node successor = arc.getDestination();
+                if (!data.isAllowed(arc)) {
+                    continue;
+                }
+
+                double oldDistance = distances[successor.getId()];
+                double newDistance = distances[node.getId()] + data.getCost(arc);
+
+                if (Double.isInfinite(oldDistance) && Double.isFinite(newDistance)) {
+                    notifyNodeReached(successor);
+                    tas.add(successor);
+                }
+
+                if (newDistance < oldDistance) {
+                    distances[successor.getId()] = newDistance;
+                    predecessorArcs[successor.getId()] = arc;
+
+                    if (tas.contains(successor)) {
+                        tas.remove(successor);
+                    }
+
+                    if (!successor.isMarque()) {
+                        heap.insert(successor);
+                        tas.add(successor);
+                        successor.setMarque(true);
+                    } else {
+                        heap.remove(successor);
+                        heap.insert(successor);
+                    }
+                }
+            }
         }
-    
-        
-        
 
-        return new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, predecessorArcs, data.getDestination(), data.getGraph()));
+
+        // Destination has no predecessor, the solution is infeasible...
+        if (predecessorArcs[data.getDestination().getId()] == null) {
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        } else {
+            // The destination has been found, notify the observers.
+            notifyDestinationReached(data.getDestination());
+
+            // Create the path from the array of predecessors...
+            ArrayList<Arc> arcs = new ArrayList<>();
+            Arc arc = predecessorArcs[data.getDestination().getId()];
+            while (arc != null) {
+                arcs.add(arc);
+                arc = predecessorArcs[arc.getOrigin().getId()];
+            }
+
+            // Reverse the path...
+            Collections.reverse(arcs);
+
+            // Create the final solution.
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+        }
+
+
+        return solution;
     }
 
 }
---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,26 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Node;
+
+public class Label implements Comparable<Label> {
+    Node sommet_courant;
+    boolean marque;
+    double cost;
+    Node father;
+
+    public Label(Node sommet_courant) {
+        this.sommet_courant = sommet_courant;
+        this.marque = false;
+        this.cost = Double.POSITIVE_INFINITY;
+        this.father = null;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+
+    @Override
+    public int compareTo(Label other) {
+        return Double.compare(this.cost, other.cost);
+    }
+}
